5aa73a8add120de6bae6086c5375bc33ce9a2133
Arthur Henrique Gunes <art123.gunesmarciano@gmail.com>
2026-06-21 18:41:29 -0300

    Documentação do Código

--- anuncio/index.php
+++ anuncio/index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"><!-- ICONES BOOTSTRAP -->
+    <link rel="stylesheet" href="assets/css/login.css"><!-- ESTILOS CSS -->
     <title>AnuncIO</title>
 </head>
 <body>
@@ -15,21 +15,21 @@
             <div class="logo">
                 <div class="icon"><i class="bi bi-megaphone"></i></div>
                 Anunc<span>IO</span>
-            </div>
+            </div><!-- LOGO -->
 
             <div class="field">
                 <div class="icon"><i class="bi bi-person"></i></div>
                 <input type="text" name="usuario" id="usuario" placeholder="Usuário" required>
-            </div>
+            </div><!-- USUÁRIO-->
             <div class="field">
                 <div class="icon"><i class="bi bi-lock"></i></div>
                 <input type="password" name="senha" id="senha" placeholder="Senha" required>
-            </div>
+            </div><!-- SENHA -->
 
-            <input type="submit" value="Entrar">
+            <input type="submit" class="btn-login" value="Entrar"><!-- BOTÃO LOGIN -->
 
             <p class="register">Ainda não possui cadastro? <a href="#">Registre-se</a></p>
-        </form><!-- FORMULÁRIO DE LOGIN -->
+        </form><!-- FORMULÁRIO LOGIN -->
 
     </main>    
 
--- anuncio/painel.php
+++ anuncio/painel.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="assets/css/sidebar.css">
-    <link rel="stylesheet" href="assets/css/painel.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"><!-- ICONES BOOTSTRAP -->
+    <link rel="stylesheet" href="assets/css/sidebar.css"><!-- ESTILO SIDEBAR -->
+    <link rel="stylesheet" href="assets/css/painel.css"><!-- ESTILO PAINEL -->
     <title>AnuncIO | Painel</title>
 </head>
 
@@ -24,7 +24,8 @@
                     <span class="name">Arthur Henrique</span>
                     <span class="user">arthurhenriquee</span>
                 </div>
-            </div>
+            </div><!-- PERFIL SIDEBAR -->
+
             <nav class="sidebar-nav">
                 <a href="#" class="nav-link active">
                     <span class="icon"><i class="bi bi-house"></i></span>
@@ -38,37 +39,40 @@
                     <span class="icon"><i class="bi bi-collection"></i></span>
                     <span class="text">Meus anúncios</span>
                 </a>
-            </nav>
+            </nav><!-- NAVEGAÇÃO SIDEBAR -->
+
             <div class="sidebar-footer">
                 <a href="#" class="logout">
                     <span class="icon"><i class="bi bi-box-arrow-left"></i></span>
                     <span class="text">Sair</span>
-                </a>
+                </a><!-- LOGOUT -->
                 <div class="logo">
                     <div class="icon">
                         <i class="bi bi-megaphone"></i>
                     </div>
                     Anunc<span>IO</span>
-                </div>
-            </div>
-        </div>
+                </div><!-- LOGO -->
+            </div><!-- RODAPÉ SIDEBAR -->
+        </div><!-- OVERFLOW -->
 
         <button class="sidebar-btn">
             <i class="bi bi-chevron-right"></i>
-        </button>
+        </button><!-- BOTÃO SIDEBAR -->
 
-    </div>
+    </div><!-- SIDEBAR -->
 
     <main>
+
         <section class="novo-anuncio">
             <div class="interface">
 
                 <div class="top-section">
                     <h2>Novo Anúncio</h2>
                     <h3>Carregue seu anúncio aqui</h3>
-                </div>
+                </div><!-- TOP SECTION -->
 
                 <form action="" method="post" class="form-novo-anuncio">
+
                     <div class="upload-image">
                         <div class="placeholder">
                             <div class="icon">
@@ -77,32 +81,33 @@
                             <div class="field">
                                 <input type="file" name="ad-image" id="ad-image" hidden>
                             </div>
-                        </div>
+                        </div><!-- PLACEHOLDER -->
 
-                        <img src="" alt="Preview de imagem" class="preview">
-                        <div class="overlay-preview"><i class="bi bi-arrow-repeat"></i></div>
-                    </div>
+                        <img src="" alt="Preview de imagem" class="preview"><!-- IMAGEM DE PREVIEW -->
+                        <div class="overlay-preview"><i class="bi bi-arrow-repeat"></i></div><!-- OVERLAY PREVIEW -->
+                    </div><!-- ÁREA DE UPLOAD -->
 
                     <div class="ad-details">
                         <div class="field">
                             <input type="text" name="ad-titulo" id="ad-titulo" required>
                             <label for="ad-titulo">Título do anúncio</label>
-                        </div>
+                        </div><!-- TÍTULO -->
                         <div class="field">
                             <input type="text" name="ad-descricao" id="ad-descricao" required>
                             <label for="ad-descricao">Descrição</label>
-                        </div>
+                        </div><!-- DESCRIÇÃO -->
 
-                        <button type="submit" id="btn-public">Publicar</button>
-                    </div>
+                        <button type="submit" id="btn-public">Publicar</button><!-- BOTÃO PUBLICAR -->
+                    </div><!-- DETALHES DO ANÚNCIO --> 
 
-                </form>
+                </form><!-- FORMULÁRIO NOVO ANÚNCIO -->
 
             </div>
-        </section>
+        </section><!-- NOVO ANÚNCIO -->
+
     </main>
 
-    <script src="assets/js/sidebar.js"></script>
+    <script src="assets/js/sidebar.js"></script><!-- SCRIPT SIDEBAR -->
     <script src="assets/js/form-newad.js"></script>
 </body>
 
